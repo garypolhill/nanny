91c38b7ea3d8cf0ddd5567d1f0d625ada0964c92
Gary Polhill <gary.polhill@hutton.ac.uk>
2019-11-29 16:57:20 +0000

    Modifications to (still incomplete) nanny.c

--- nanny.c
+++ nanny.c
@@ -1,7 +1,24 @@
 /* nanny.c
  *
+ * Gary Polhill, 28 November 2019
+ *
  * C program to 'nanny' a command you want to run and give you information about
  * its termination and any signals it got sent.
+ *
+ * Copyright (C) 2019  The James Hutton Institute
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <stdlib.h>
@@ -121,9 +138,11 @@ int main(int argc, char **argv) {
   else if(child_pid == -1) {
     /* error */
     perror("fork failed");
+    exit(1);
   }
   else {
     /* parent */
+    
   }
 }
 
